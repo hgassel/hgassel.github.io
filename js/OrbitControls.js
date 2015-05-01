72483006bbddb191ffeb80dde9634ac80e7fdd5a
Ryan <ryan@superpositionkitty.com>
2015-05-01 21:22:52 +0100

    changes as discussed on skype

--- js/OrbitControls.js
+++ js/OrbitControls.js
@@ -435,10 +435,10 @@ THREE.OrbitControls = function ( object, domElement ) {
 			rotateDelta.subVectors( rotateEnd, rotateStart );
 
 			// rotating across whole screen goes 360 degrees around
-			scope.rotateLeft( 2 * Math.PI * rotateDelta.x / element.clientWidth/7 * scope.rotateSpeed );
+			scope.rotateLeft( 2 * Math.PI * (rotateDelta.x / element.clientWidth/4) * scope.rotateSpeed );
 
 			// rotating up and down along whole screen attempts to go 360, but limited to 180
-			scope.rotateUp( 2 * Math.PI * rotateDelta.y / element.clientHeight/7 * scope.rotateSpeed );
+			scope.rotateUp( 2 * Math.PI * (rotateDelta.y / element.clientHeight/7) * scope.rotateSpeed );
 
 			rotateStart.copy( rotateEnd );
 
