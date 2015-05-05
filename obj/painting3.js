732a495858fb68805f3a6249589a527d370910c0
Ryan <ryan@superpositionkitty.com>
2015-05-05 20:19:58 +0100

    try with bin loader

---
+++ obj/painting3.js
@@ -0,0 +1,29 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "PAINTING_3a.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 196608,
+        "faces"         : 388954,
+        "normals"       : 0,
+        "uvs"           : 196618,
+        "materials"     : 2
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "defaultMat"
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "Wood_Fraxinus"
+	}],
+
+    "buffers": "painting3.bin"
+
+}
