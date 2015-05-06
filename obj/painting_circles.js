3ddefea3feebdd764eb743616967866943a7fec8
Ryan <ryan@superpositionkitty.com>
2015-05-06 18:53:18 +0100

    new version, all paintings, mobile views, ipad fixes, bug fixes, copy changes, credits etc

---
+++ obj/painting_circles.js
@@ -0,0 +1,29 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "painting-4.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 26460,
+        "faces"         : 51075,
+        "normals"       : 0,
+        "uvs"           : 153031,
+        "materials"     : 2
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "Wood_Fraxinus"
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "defaultMat"
+	}],
+
+    "buffers": "painting_circles.bin"
+
+}
---
+++ obj/painting_freeform.js
@@ -0,0 +1,23 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "painting-2-daea2c.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 31284,
+        "faces"         : 58333,
+        "normals"       : 0,
+        "uvs"           : 174576,
+        "materials"     : 1
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "defaultMat"
+	}],
+
+    "buffers": "painting_freeform.bin"
+
+}
